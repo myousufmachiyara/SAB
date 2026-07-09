bedec87393e43fb839be0b20503225c9d86cbf3a
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-07-09 09:01:15 +0500

    Update SaleInvoiceController.php

--- app/Http/Controllers/SaleInvoiceController.php
+++ app/Http/Controllers/SaleInvoiceController.php
@@ -161,7 +161,7 @@ class SaleInvoiceController extends Controller
     public function index()
     {
         $invoices = SaleInvoice::with('items.product', 'account')
-            ->latest('invoice_date') // change to 'date' if that's your actual column
+            ->latest('date') // change to 'date' if that's your actual column
             ->get();
         return view('sales.index', compact('invoices'));
     }
